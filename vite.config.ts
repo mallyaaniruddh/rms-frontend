35c7437dd897106b17ce9ac19ac4e9f9e910f732
mallyaaniruddh <24124133@studentmail.ul.ie>
2025-03-12 15:06:27 +0000

    Updated features

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/rms-frontend/",  // this matches GitHub repo name
-  define: {
-    "VITE_BASE_URL": '"/rms-frontend/"',  // Ensures proper asset paths
-  }
 });
